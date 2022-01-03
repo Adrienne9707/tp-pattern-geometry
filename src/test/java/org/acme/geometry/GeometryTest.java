7dc735d2cea9c5e6c0ee6cc942fbb786cecbc02a
Adry97 <lindsayzebazeadrienne@gmail.com>
2022-01-03 16:05:18 +0100

    add test for linestring

--- src/test/java/org/acme/geometry/GeometryTest.java
+++ src/test/java/org/acme/geometry/GeometryTest.java
@@ -3,6 +3,9 @@ package org.acme.geometry;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GeometryTest {
 
     public static final double EPSILON = 1.0e-15;
@@ -10,12 +13,18 @@ public class GeometryTest {
 	@Test
 	public void testDefaultConstructor(){
 		// TODO
-		Point p = new Point(); 
-        LineString l = new LineString();
-		Assert.assertEquals(0.0, p.getCoordinate().getX(), EPSILON);
-		Assert.assertEquals(0.0, p.getCoordinate().getY(), EPSILON);
-        Assert.assertEquals(2, l.getNumPoints(), EPSILON);
+        Coordinate c = new Coordinate(3.5, 6.6);
+		Point p = new Point(c); 
         
+        List<Point> points = new ArrayList<>();
+        points.add(p);
+        
+        LineString l = new LineString(points);
+        
+		//Assert.assertEquals(3.5, p.getCoordinate().getX(), EPSILON);
+		//Assert.assertEquals(6.6, p.getCoordinate().getY(), EPSILON);
+        Assert.assertEquals(1, l.getNumPoints());
+        Assert.assertEquals(p, l.getPointN(0));
 		
 	}
 }
