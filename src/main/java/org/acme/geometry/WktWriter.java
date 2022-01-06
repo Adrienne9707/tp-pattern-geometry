814d8c11ea2d00d1a6e1e8d6e4e6a4019be10dd6
AdrienneZebaze <Adrienne-Lindsay.Zebaze-Voukeng@ensg.eu>
2022-01-06 23:57:09 +0100

    comment all write method

--- src/main/java/org/acme/geometry/WktWriter.java
+++ src/main/java/org/acme/geometry/WktWriter.java
@@ -4,25 +4,25 @@ package org.acme.geometry;
 public class WktWriter {
 
 
-    public String write(Geometry geometry){
+    /*public String write(Geometry geometry){
         //geometry = new Point(new Coordinate(this.coordinate.getX(),this.coordinate.getY()));
         
 
         if ( geometry instanceof Point ){
             Point point = (Point) geometry;
-            LineString line = (LineString) geometry;
             
-            return system.out.printl("Point("+point.getCoordinate().getX() +
-            point.getCoordinate().getY() ")");
+            return system.out.printl("Point(" + point.getCoordinate().getX() 
+            + "" + point.getCoordinate().getY() + ")");
 
         }else if ( geometry instanceof LineString ){
-            LineString lineString = (LineString)geometry;
+            //LineString lineString = (LineString)geometry;
+            LineString line = (LineString) geometry;
             return system.out.printl("LineString("+line.getCoordinate().getX() +
             line.getCoordinate().getY()+ "," +line.getCoordinate().getX() +
             line.getCoordinate().getY()+ ")" );
-            
+    
         }else{
             throw new RuntimeException("geometry type not supported");
         }
-    }
+    }*/
 }
